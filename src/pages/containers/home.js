b45b365d36ebfb624919ac7a55c0ff67142cf1fa
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-12-11 08:33:18 -0300

    20 - Estructura del proyecto con containers y UI components: Contenido relacionado y estilos

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,11 +3,13 @@ import React, {
 } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
+import Related from '../components/related-layout';
 
 class Home extends Component {
   render() {
     return (
       <HomeLayout>
+        <Related />
         <Categories categories={this.props.data.categories}/>
       </HomeLayout>
     );
