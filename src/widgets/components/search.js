0e0c7398f9f29da0bbd5397fc823c836ff4c2548
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-12-23 20:25:36 -0300

    24 - UI de Formularios

---
+++ src/widgets/components/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search.css';
+
+const Search = () => (
+  <form className="Search">
+    <input
+      type="search"
+      placeholder="Busca tu video favorito"
+      className="Search-input"
+      />
+  </form>
+);
+
+export default Search;
---
+++ src/widgets/containers/search.js
@@ -0,0 +1,14 @@
+import React, {
+  Component
+} from 'react';
+import Search from '../components/search';
+
+class SearchContainer extends Component {
+  render() {
+    return (
+      <Search />
+    );
+  }
+}
+
+export default Search;
