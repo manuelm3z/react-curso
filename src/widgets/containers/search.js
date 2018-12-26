1a97e90153ecbdfc2594a27562d6ac2d08a639ed
Manuel Zambrano <manuel.zambrano@stensul.com>
2018-12-26 08:21:03 -0300

    25 - Referencias a Eventos de HTML y Formularios

--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -4,11 +4,31 @@ import React, {
 import Search from '../components/search';
 
 class SearchContainer extends Component {
+  state = {
+    value: 'Luis Fonsi'
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.input.value, 'submit');
+  }
+  setInputRef = element => {
+    this.input = element;
+  }
+  handlInputChange = event => {
+    this.setState({
+      value: event.target.value.replace(' ', '-')
+    });
+  }
   render() {
     return (
-      <Search />
+      <Search
+        setRef={this.setInputRef}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handlInputChange}
+        value={this.state.value}
+        />
     );
   }
 }
 
-export default Search;
+export default SearchContainer;
